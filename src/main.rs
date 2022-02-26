7ac7f791e06fcced70ef243f0b6227b84c7a8697
ryu-min <SergeyR6@yandex.ru>
2022-02-27 02:32:35 +0300

    arctic basics

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,42 @@ use to_do::structs::traits::create::Create;
 
 use processes::process_input;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+use actix_web::{web, App, Responder, HttpServer, HttpRequest};
 
-    let command: &String = &args[1];
-    let title: &String = &args[2];
-
-    let state: Map<String, Value> = read_file("/home/z1p1t/workspace/rust_web/state.json");
-    let status : String;
-    match &state.get(*&title) {
-        Some(result) => {
-            status = result.to_string().replace('\"', "");
-        },
-        None => {
-            status = String::from("pending");
-        }
-    }
-
-    let item = to_do_factory(&status, title).expect(&status);
-    process_input(item, command, &state);
+async fn greet(req : HttpRequest) -> impl Responder{
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello, {}", name)
 }
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
+
+// fn main() {
+//     let args: Vec<String> = env::args().collect();
+//
+//     let command: &String = &args[1];
+//     let title: &String = &args[2];
+//
+//     let state: Map<String, Value> = read_file("/home/z1p1t/workspace/rust_web/state.json");
+//     let status : String;
+//     match &state.get(*&title) {
+//         Some(result) => {
+//             status = result.to_string().replace('\"', "");
+//         },
+//         None => {
+//             status = String::from("pending");
+//         }
+//     }
+//
+//     let item = to_do_factory(&status, title).expect(&status);
+//     process_input(item, command, &state);
+// }
