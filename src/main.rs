55a8450a87a50cc1d81f94d2819ad9d2c7b2614f
ryu-min <SergeyR6@yandex.ru>
2022-02-27 03:08:07 +0300

    thread basics

--- src/main.rs
+++ src/main.rs
@@ -15,22 +15,59 @@ use processes::process_input;
 
 use actix_web::{web, App, Responder, HttpServer, HttpRequest};
 
-async fn greet(req : HttpRequest) -> impl Responder{
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello, {}", name)
+// fn main() {
+//     let test = "test";
+//     let test_function = || {
+//         println!("test value is {}", test);
+//         return test.to_owned() + &String::from(" case");
+//     };
+//
+//     let outcome : String =  test_function();
+//     println!("outcome value is {}", outcome);
+// }
+
+use std::{thread, time};
+use std::thread::JoinHandle;
+
+fn do_something(number : i8) -> i8 {
+    println!("number is {}", number);
+    let two_second = time::Duration::new(2, 0);
+    thread::sleep(two_second);
+    2
 }
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+fn main() {
+    let now = time::Instant::now();
+    let one_thread    : JoinHandle<i8> = thread::spawn(|| do_something(1));
+    let two_thread    : JoinHandle<i8> = thread::spawn(|| do_something(2));
+    let three_thread  : JoinHandle<i8> = thread::spawn(|| do_something(3));
+
+    let result_one = one_thread.join();
+    let result_two = two_thread.join();
+    let result_three = three_thread.join();
+
+    println!("time elapsed: {:?}", now.elapsed());
+    println!("result: {}", result_one.unwrap() + result_two.unwrap() + result_three.unwrap());
 }
+
+// async fn greet(req : HttpRequest) -> impl Responder{
+//     let name = req.match_info().get("name").unwrap_or("World");
+//     format!("Hello, {}", name)
+// }
+//
+// #[actix_rt::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         println!("function is firing");
+//         let app  = App::new()
+//             .route("/", web::get().to(greet))
+//             .route("/{name}", web::get().to(greet));
+//         return app;
+//     })
+//         .bind("127.0.0.1:8000")?
+//         .run()
+//         .await
+// }
 
 // fn main() {
 //     let args: Vec<String> = env::args().collect();
